6b8c9fa7d54bbd28988c7671bf9600575c01ebbe
Leonardo Monteiro <leonardo.monteiro@sptech.school>
2025-11-22 22:36:01 -0300

    add: captura de dados de disco

--- aplicacaoDeCaptura.py
+++ aplicacaoDeCaptura.py
@@ -18,21 +18,22 @@ def insercao():
             print('Connected to MySQL server')
             
             with db.cursor() as cursor:
-                cpu_percent = p.cpu_percent(interval=1)
-                ram_percent = p.virtual_memory().percent
-                disco_percent = p.disk_usage("/").percent
                 
-                print(f"Dados capturados:")
-                print(f"  CPU: {cpu_percent:.1f}%")
-                print(f"  RAM: {ram_percent:.1f}%") 
-                print(f"  DISCO: {disco_percent:.1f}%")
-                
-                # Inserir cada componente
                 fk_comp1 = "1"
                 fk_comp2 = "2"
                 fk_comp3 = "3"
                 
                 while True:
+
+                    cpu_percent = p.cpu_percent(interval=1)
+                    ram_percent = p.virtual_memory().percent
+                    disco_percent = p.disk_usage("/").percent
+                    
+                    print(f"Dados capturados:")
+                    print(f"  CPU: {cpu_percent:.1f}%")
+                    print(f"  RAM: {ram_percent:.1f}%") 
+                    print(f"  DISCO: {disco_percent:.1f}%")
+
                     query = """INSERT INTO Registro(captura, fkComponente) 
                               VALUES (%s, %s)"""
                     cursor.execute(query, (cpu_percent, fk_comp1))
@@ -56,8 +57,45 @@ def insercao():
                     
                     db.commit()
                     print("Inserindo Registros")
-            
-                    time.sleep(2)
+
+                    io_before = p.disk_io_counters()
+                    time.sleep(1)
+                    io_after = p.disk_io_counters()
+
+                    taxaLeitura = (io_after.read_bytes - io_before.read_bytes) / 1024 / 1024
+                    taxaEscrita = (io_after.write_bytes - io_before.write_bytes) / 1024 / 1024
+
+                    processos = []
+                    for proc in p.process_iter(['name', 'memory_info']):
+                        try:
+                            uso = proc.info['memory_info'].rss / 1024 / 1024
+                            processos.append((proc.info['name'], uso))
+                        except:
+                            pass
+
+                    processos.sort(key=lambda x: x[1], reverse=True)
+                    top1, top2, top3 = processos[:3]
+
+                    query = """
+                        INSERT INTO registroDisco
+                        (fkMaquina, taxaLeitura, taxaEscrita,
+                        top1, top1Valor,
+                        top2, top2Valor,
+                        top3, top3Valor)
+                        VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                    """
+
+                    cursor.execute(query, (
+                        "1",
+                        taxaLeitura, taxaEscrita,
+                        top1[0], top1[1],
+                        top2[0], top2[1],
+                        top3[0], top3[1]
+                    ))
+
+                    db.commit()
+                    print("registroDisco inserido")
+
 
             cursor.close()
             db.close()
