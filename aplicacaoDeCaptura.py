5ce739d81c14139d5f4bf24cfb3961f59f001c1b
Thiago Prates <thiago.prates@sptech.school>
2025-11-26 23:10:02 -0300

    fix: inserção

--- aplicacaoDeCaptura.py
+++ aplicacaoDeCaptura.py
@@ -58,79 +58,79 @@ def insercao():
 
                     ##-------------- Coleta de Disco ---------------##
 
-            #         io_before_total = p.disk_io_counters(perdisk=False)
-            #         time.sleep(1)
-            #         io_after_total = p.disk_io_counters(perdisk=False)
+                    io_before_total = p.disk_io_counters(perdisk=False)
+                    time.sleep(1)
+                    io_after_total = p.disk_io_counters(perdisk=False)
 
-            #         taxaLeitura = (io_after_total.read_bytes - io_before_total.read_bytes) / 1024 / 1024
-            #         taxaEscrita = (io_after_total.write_bytes - io_before_total.write_bytes) / 1024 / 1024
+                    taxaLeitura = (io_after_total.read_bytes - io_before_total.read_bytes) / 1024 / 1024
+                    taxaEscrita = (io_after_total.write_bytes - io_before_total.write_bytes) / 1024 / 1024
 
-            #         processos = []
-            #         for proc in p.process_iter(['name', 'memory_info']):
-            #             try:
-            #                 uso = proc.info['memory_info'].rss / 1024 / 1024
-            #                 processos.append((proc.info['name'], uso))
-            #             except:
-            #                 pass
+                    processos = []
+                    for proc in p.process_iter(['name', 'memory_info']):
+                        try:
+                            uso = proc.info['memory_info'].rss / 1024 / 1024
+                            processos.append((proc.info['name'], uso))
+                        except:
+                            pass
 
-            #         processos.sort(key=lambda x: x[1], reverse=True)
-            #         top1, top2, top3 = processos[:3]
+                    processos.sort(key=lambda x: x[1], reverse=True)
+                    top1, top2, top3 = processos[:3]
 
-            #         processosLeitura = []
-            #         processosEscrita = []
+                    processosLeitura = []
+                    processosEscrita = []
 
-            #         for proc in p.process_iter(['name', 'io_counters']):
-            #             try:
-            #                 io = proc.info['io_counters']
-            #                 if io:
-            #                     processosLeitura.append((proc.info['name'], io.read_bytes))
-            #                     processosEscrita.append((proc.info['name'], io.write_bytes))
-            #             except:
-            #                 pass
+                    for proc in p.process_iter(['name', 'io_counters']):
+                        try:
+                            io = proc.info['io_counters']
+                            if io:
+                                processosLeitura.append((proc.info['name'], io.read_bytes))
+                                processosEscrita.append((proc.info['name'], io.write_bytes))
+                        except:
+                            pass
 
-            #         processosLeitura.sort(key=lambda x: x[1], reverse=True)
-            #         processosEscrita.sort(key=lambda x: x[1], reverse=True)
+                    processosLeitura.sort(key=lambda x: x[1], reverse=True)
+                    processosEscrita.sort(key=lambda x: x[1], reverse=True)
 
-            #         procMaisLeitura, procMaisLeituraValor = processosLeitura[0]
-            #         procMaisEscrita, procMaisEscritaValor = processosEscrita[0]
+                    procMaisLeitura, procMaisLeituraValor = processosLeitura[0]
+                    procMaisEscrita, procMaisEscritaValor = processosEscrita[0]
 
-            #         queryRegistro = """INSERT INTO Registro(captura, fkComponente) VALUES (%s, %s)"""
-            #         cursor.execute(queryRegistro, (cpu_percent, fk_comp1))
-            #         db.commit()
+                    queryRegistro = """INSERT INTO Registro(captura, fkComponente) VALUES (%s, %s)"""
+                    cursor.execute(queryRegistro, (cpu_percent, fk_comp1))
+                    db.commit()
 
-            #         cursor.execute(queryRegistro, (ram_percent, fk_comp2))
-            #         db.commit()
+                    cursor.execute(queryRegistro, (ram_percent, fk_comp2))
+                    db.commit()
 
-            #         cursor.execute(queryRegistro, (disco_percent, fk_comp3))
-            #         db.commit()
+                    cursor.execute(queryRegistro, (disco_percent, fk_comp3))
+                    db.commit()
 
-            #         query = """
-            #             INSERT INTO registroDisco
-            #             (fkMaquina, taxaLeitura, taxaEscrita,
-            #             top1, top1Valor,
-            #             top2, top2Valor,
-            #             top3, top3Valor,
-            #             procMaisLeitura, procMaisLeituraValor,
-            #             procMaisEscrita, procMaisEscritaValor)
-            #             VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-            #         """
+                    # query = """
+                    #     INSERT INTO registroDisco
+                    #     (fkMaquina, taxaLeitura, taxaEscrita,
+                    #     top1, top1Valor,
+                    #     top2, top2Valor,
+                    #     top3, top3Valor,
+                    #     procMaisLeitura, procMaisLeituraValor,
+                    #     procMaisEscrita, procMaisEscritaValor)
+                    #     VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+                    # """
 
 
-            #         cursor.execute(query, (
-            #             "1",
-            #             taxaLeitura, taxaEscrita,
-            #             top1[0], top1[1],
-            #             top2[0], top2[1],
-            #             top3[0], top3[1],
-            #             procMaisLeitura, procMaisLeituraValor,
-            #             procMaisEscrita, procMaisEscritaValor
-            #         ))
-            #         db.commit()
+                    # cursor.execute(query, (
+                    #     "1",
+                    #     taxaLeitura, taxaEscrita,
+                    #     top1[0], top1[1],
+                    #     top2[0], top2[1],
+                    #     top3[0], top3[1],
+                    #     procMaisLeitura, procMaisLeituraValor,
+                    #     procMaisEscrita, procMaisEscritaValor
+                    # ))
+                    # db.commit()
 
-            #         print("registroDisco inserido")
+                    # print("registroDisco inserido")
 
-            # cursor.close()
-            # db.close()
+            cursor.close()
+            db.close()
 
     except Error as e:
         print('Error to connect with MySQL -', e)
